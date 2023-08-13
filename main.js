2d89a13c4aef1b39519637292fc163d23b4a0069
Floupy <tmurnieks@gmail.com>
2023-08-13 23:42:27 +0300

    implemented option to choose between black and random colour ink; for now only works before a grid is created

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 const container = document.querySelector('.container');
 const createGridButton = document.querySelector('.createGrid');
 const clearGridButton = document.querySelector('.clearGrid');
-const chooseGridSizeButton = document.querySelector('.gridSizeChooser');
+const toggleBlackInk = document.querySelector('.blackInk');
+const toggleColourfulInk = document.querySelector('.colourfulInk');
 let grid;
 let gridRows = 16;
 let gridCols = 16;
@@ -12,7 +13,10 @@ let maxCellSize = 0;
 
 createGridButton.addEventListener('click', createGrid);
 clearGridButton.addEventListener('click', clearGrid);
-chooseGridSizeButton.addEventListener('click', createGrid);
+toggleBlackInk.addEventListener('click', drawBlack);
+toggleColourfulInk.addEventListener('click', drawColourful);
+
+// TODO make ink colour choosable during drawing
 function createGrid() {
     clearGrid();
     getGridSize();
@@ -22,7 +26,13 @@ function createGrid() {
         for(let i = 0; i < gridSize * gridSize; i++) {
             grid = document.createElement('div');
             grid.className = 'grid';
-            grid.addEventListener('mouseover', draw);
+            if(container.classList.contains('blackInk')) {
+                grid.addEventListener('mouseover', drawBlack);
+            }
+            else if(container.classList.contains('colourfulInk')) {
+                grid.addEventListener('mouseover', drawColourful)
+            }
+            // grid.addEventListener('mouseover', drawColourful);
             grid.style.maxHeight = maxCellSize;
             grid.style.flexBasis = maxCellSize + "px";
             container.appendChild(grid);
@@ -34,8 +44,20 @@ function clearGrid() {
         container.lastChild.remove();
     }
 }
-function draw (){
+function drawBlack (){
+    container.classList.remove('colourfulInk');
+    container.classList.add('blackInk');
     this.style.backgroundColor = 'black';
+}
+
+function drawColourful() {
+    container.classList.remove('blackInk');
+    container.classList.add('colourfulInk');
+    let redValue = Math.floor(Math.random() * 256);
+    let greenValue = Math.floor(Math.random() * 256);
+    let blueValue = Math.floor(Math.random() * 256);
+    console.log();
+    this.style.backgroundColor = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
 }
 
 function getGridSize() {
