b60ecc108d30fb4d0ed678899dee3af1c156b73a
Floupy <tmurnieks@gmail.com>
2023-08-15 13:49:41 +0300

    Implemented ability to switch colours while drawing without having to create new grid, cleaned up code, renamed 'grid' class to 'cell' class

--- main.js
+++ main.js
@@ -3,61 +3,64 @@
 const container = document.querySelector('.container');
 const createGridButton = document.querySelector('.createGrid');
 const clearGridButton = document.querySelector('.clearGrid');
-const toggleBlackInk = document.querySelector('.blackInk');
+const toggleBlackInkButton = document.querySelector('.blackInk');
 const toggleColourfulInk = document.querySelector('.colourfulInk');
-let grid;
-let gridRows = 16;
-let gridCols = 16;
+let cell = document.querySelector('.cell');
 let gridSize = 16
 let maxCellSize = 0;
 
 createGridButton.addEventListener('click', createGrid);
 clearGridButton.addEventListener('click', clearGrid);
-toggleBlackInk.addEventListener('click', drawBlack);
+toggleBlackInkButton.addEventListener('click', drawBlack);
 toggleColourfulInk.addEventListener('click', drawColourful);
 
-// TODO make ink colour choosable during drawing
 function createGrid() {
     clearGrid();
     getGridSize();
     maxCellSize = (960 / gridSize);
-    console.log(maxCellSize);
-
         for(let i = 0; i < gridSize * gridSize; i++) {
-            grid = document.createElement('div');
-            grid.className = 'grid';
-            if(container.classList.contains('blackInk')) {
-                grid.addEventListener('mouseover', drawBlack);
-            }
-            else if(container.classList.contains('colourfulInk')) {
-                grid.addEventListener('mouseover', drawColourful)
-            }
-            // grid.addEventListener('mouseover', drawColourful);
-            grid.style.maxHeight = maxCellSize;
-            grid.style.flexBasis = maxCellSize + "px";
-            container.appendChild(grid);
+            cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.classList.add('blackInk');
+            cell.addEventListener('mouseover', draw);
+            cell.style.flexBasis = maxCellSize + "px";
+            container.appendChild(cell);
         }
-
 }
 function clearGrid() {
-    while(container.hasChildNodes()) {
-        container.lastChild.remove();
-    }
-}
-function drawBlack (){
-    container.classList.remove('colourfulInk');
-    container.classList.add('blackInk');
-    this.style.backgroundColor = 'black';
+    container.innerText = " ";
 }
 
-function drawColourful() {
-    container.classList.remove('blackInk');
-    container.classList.add('colourfulInk');
+function draw() {
     let redValue = Math.floor(Math.random() * 256);
     let greenValue = Math.floor(Math.random() * 256);
     let blueValue = Math.floor(Math.random() * 256);
-    console.log();
-    this.style.backgroundColor = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+
+    if(cell.classList.contains('blackInk')) {
+        this.style.backgroundColor = 'black';
+
+    }
+    else if(cell.classList.contains('colourfulInk')) {
+        this.style.backgroundColor = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+    }
+}
+
+
+function drawBlack (){
+    if(!cell.classList.contains('colourfulInk')) {
+        cell.classList.add('blackInk')
+    } else if (cell.classList.contains('colourfulInk')) {
+        cell.classList.replace('colourfulInk', 'blackInk');
+    }
+
+}
+
+function drawColourful() {
+    if(!cell.classList.contains('blackInk')) {
+        cell.classList.add('colourfulInk');
+    } else if (cell.classList.contains('blackInk')) {
+        cell.classList.replace('blackInk', 'colourfulInk');
+    }
 }
 
 function getGridSize() {
@@ -68,5 +71,4 @@ function getGridSize() {
     else {
         return gridSize;
     }
-
 }
